6454b41102f978b72e06918353e41db1133b34d2
Juan Rabaglia <raba_emp@hotmail.com>
2018-02-03 14:22:58 -0300

    Added lists section

--- index.php
+++ index.php
@@ -71,6 +71,34 @@ get_header();
 				</div>
 			</div>
 		</section>
+		<section id="section-lists">
+			<div class="section-content container">
+				<div class="list-container" id="landing-list-1">
+					<h6 class="list-title">STRATEGY</h6>
+					<ul>
+						<li>BRANDING</li>
+						<li>NAMING ID</li>
+						<li>INSIGHTS AND DATA</li>
+						<li>RESEARCH</li>
+						<li>BRAND</li>
+						<li>BUSINESS STRATEGY</li>
+						<li>DIGITAL INSIGHT</li>
+						<li>CONCEPT DEVELOPMENT</li>
+						<li>CONTENT</li>
+					</ul>
+				</div>
+				<div class="list-container" id="landing-list-2">
+					<h6 class="list-title">DESIGN</h6>
+					<ul>
+						<li>PACKAGING</li>
+						<li>VISUALS</li>
+						<li>ILLUSTRATION</li>
+						<li>UX</li>
+						<li>DIGITAL CRAFT</li>				
+					</ul>
+				</div>			
+			</div>
+		</section>
 	</div>
 <script>
 $(document).ready( function(){
