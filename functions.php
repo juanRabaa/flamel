41c13856cf2b5387689ca20d7687fe1c8054cd0e
Juan Rabaglia <raba_emp@hotmail.com>
2018-02-06 15:41:40 -0300

    Added customizer js and php, created sections files and sections order control

--- functions.php
+++ functions.php
@@ -15,3 +15,41 @@ function load_scripts() {
 	wp_enqueue_script( "dynamics", get_template_directory_uri()."/js/libs/dynamics.min.js", true );
 }
 add_action ("wp_enqueue_scripts", "load_scripts");
+
+function load_styles_customizer() {	
+	wp_enqueue_style( "skeleton-css", get_template_directory_uri()."/css/libs/Skeleton-2.0.4/css/skeleton.css", array() );
+	wp_enqueue_style( "font-awesome-css", get_template_directory_uri()."/css/libs/font-awesome-4.7.0/css/font-awesome.min.css", array() );		
+	wp_enqueue_style( "customizer-css", get_template_directory_uri()."/css/src/customizer.css" );
+	wp_enqueue_style( "customizer-image-selection-control", get_template_directory_uri()."/css/src/customizer-image-selection-control.css" );
+	wp_enqueue_style( "customizer-color-scheme-control", get_template_directory_uri()."/css/src/customizer-color-scheme-control.css" );
+	wp_enqueue_style( "customizer-sortable-list-control", get_template_directory_uri()."/css/src/customizer-sortable-list-control.css" );
+}
+add_action( 'customize_controls_enqueue_scripts', 'load_styles_customizer' );	
+
+function load_script_customizer() {
+		wp_enqueue_script( 'jquery-sortable', get_template_directory_uri() . '/js/libs/jquery-sortable.js', array( 'jquery' ), rand(), true );
+		wp_enqueue_script( 'wp-editor-customizer', get_template_directory_uri() . '/js/src/customizer-panel.js', array( 'jquery' ), rand(), true );
+		wp_enqueue_script( "customizer-image-selection-control", get_template_directory_uri()."/js/src/customizer-image-selection-control.js", array("jquery"), true );
+		wp_enqueue_script( "customizer-sortable-list-control", get_template_directory_uri()."/js/src/customizer-sortable-list-control.js", array("jquery"), true );
+}
+add_action( 'customize_controls_enqueue_scripts', 'load_script_customizer' );
+
+require 'customizer.php';
+
+function get_sections_order(){
+	return explode(',', get_theme_mod('sections-order'));
+}
+
+function load_frontpage_sections(){
+	$sections_order = get_sections_order();
+	?>
+	<div id="main-sections">
+	<?php
+	get_template_part( "sections/section", "intro" );
+	foreach( $sections_order as $section_sufix ){
+		get_template_part( "sections/section", $section_sufix );
+	}
+	?>
+	</div>
+	<?php
+}
